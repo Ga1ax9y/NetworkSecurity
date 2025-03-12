b97d1e20e61cb1acef8979682899eb2fb381a6d6
Ga1ax9y <sasha20050803@gmail.com>
2025-03-12 23:52:53 +0300

    lab5 added v1.0

---
+++ lr5/ISOB_5/ISOB_5/vulnerable.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <cstring>
+#include <iomanip>
+
+using namespace std;
+
+void printMemory(const char* buffer, int size, const char* label) {
+    cout << "\n=== Memory Dump (" << label << ") ===\n";
+    for (int i = 0; i < size+16; i++) {
+        cout << "Address: " << (void*)&buffer[i]
+            << " | Value: " << hex << (int)buffer[i]
+            << dec << " | Char: " << buffer[i] << endl;
+    }
+}
+
+void demo() {
+    char buffer[16];
+    int isAdmin = 0;
+
+    memset(buffer, 0, sizeof(buffer));
+
+    cout << "\033[1;31m" << "[VULNERABLE MODE]" << "\033[0m\n";
+    cout << "Buffer Address: " << (void*)buffer << endl;
+    cout << "isAdmin Address: " << (void*)&isAdmin << endl;
+    cout << "Initial isAdmin: " << isAdmin << "\n\n";
+
+    cout << "Enter input: ";
+    cin.getline(buffer, 255); 
+
+    cout << "\n\033[1;31m" << "[AFTER OVERFLOW]" << "\033[0m\n";
+    cout << "Buffer Content: \"" << buffer << "\"\n";
+    cout << "isAdmin Value: " << isAdmin << endl;
+    printMemory(buffer, 32, "Buffer + Overflow");
+
+    if (isAdmin) {
+        cout << "\n\033[1;31m" << "EXPLOIT SUCCESSFUL! Admin access granted.\033[0m\n";
+    }
+    else {
+        cout << "\nNormal user access.\n";
+    }
+}
+
+int main() {
+    demo();
+    return 0;
+}
---
+++ lr5/ISOB_5/PROTECTED/protected.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <cstring>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void bufferSizeLimit() {
+    char buffer[16];
+    int isAdmin = 0;
+
+    cout << "\033[1;32m" << "[PROTECTION: BUFFER SIZE LIMIT]" << "\033[0m\n";
+    cout << "Buffer Address: " << (void*)buffer << endl;
+    cout << "isAdmin Address: " << (void*)&isAdmin << endl;
+
+    cout << "Enter input: ";
+    cin.getline(buffer, sizeof(buffer));
+
+    cout << "\n\033[1;32m" << "[AFTER PROTECTED INPUT]" << "\033[0m\n";
+    cout << "Buffer Content: \"" << buffer << "\"\n";
+    cout << "isAdmin Value: " << isAdmin << endl;
+
+    if (isAdmin) {
+        cout << "Admin access granted.\n";
+    }
+    else {
+        cout << "Normal user access.\n";
+    }
+}
+
+void stackCanary() {
+    char buffer[16];
+    unsigned long canary = 0;
+    int isAdmin = 0;
+
+    srand(time(0));
+    canary = rand();
+    unsigned long canaryPtr = canary;
+
+    cout << "\033[1;32m" << "[PROTECTION: STACK CANARY]" << "\033[0m\n";
+    cout << "Buffer Address: " << (void*)buffer << endl;
+    cout << "Canary Address: " << (void*)canaryPtr << endl;
+    cout << "isAdmin Address: " << (void*)&isAdmin << endl;
+    cout << "Canary Value: " << hex << canary << dec << "\n";
+
+    cout << "Enter input: ";
+    cin.getline(buffer, 255);
+
+
+    if (canary != canaryPtr) {
+        cout << "\033[1;31m" << "[STACK CANARY CORRUPTED!]" << "\033[0m\n";
+        cout << "Buffer overflow detected. Terminating program.\n";
+        exit(1); 
+    }
+
+    cout << "\n\033[1;32m" << "[AFTER PROTECTED INPUT]" << "\033[0m\n";
+    cout << "Buffer Content: \"" << buffer << "\"\n";
+    cout << "isAdmin Value: " << isAdmin << endl;
+
+    if (isAdmin) {
+        cout << "Admin access granted.\n";
+    }
+    else {
+        cout << "Normal user access.\n";
+    }
+}
+
+
+int main() {
+    int choice;
+
+    cout << "Choose a protection method:\n";
+    cout << "1. Buffer Size Limitation\n";
+    cout << "2. Stack Canary\n";
+    cout << "Enter your choice (1-2): ";
+    cin >> choice;
+    cin.ignore();
+
+    switch (choice) {
+    case 1:
+        bufferSizeLimit();
+        break;
+    case 2:
+        stackCanary();
+        break;
+    default:
+        cout << "Invalid choice. Exiting.\n";
+        break;
+    }
+
+    return 0;
+}
